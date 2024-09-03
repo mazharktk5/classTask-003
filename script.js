fa11d721c29385231095c83522b734a36f2c769b
https://github.com/mazharktk5 <mazharahmad862@gmail.com>
2024-09-03 00:12:51 -0700

    added ann array of question objects with options

---
+++ script.js
@@ -0,0 +1,51 @@
+var questions = [
+    {
+        question: 'Which country has the most islands?',
+        option: ['Finland', 'Norway', 'Sweden', 'Denmark'],
+        answer: 2
+    },
+
+    {
+        question: 'What is the capital city of France?',
+        option: ['Paris', 'London', 'Madrid', 'Berlin'],
+        answer: 0
+    },
+
+    {
+        question: 'Who is the current Prime Minister of the United Kingdom?',
+        option: ['Boris Johnson', 'Theresa May', 'David Cameron', 'George Osborne'],
+        answer: 1
+    },
+
+    {
+        question: 'What is the capital city of Japan?',
+        option: ['Tokyo', 'Osaka', 'Kyoto', 'Hakone'],
+        answer: 0
+    },
+   
+
+
+    {
+        question: 'Which is the most popular front-end framework?',
+        option: ['React', 'Vue.js', 'Angular', 'Bootstrap'],
+        answer: 1
+    },
+    {
+        question: 'Which programming language is primarily used for web development?',
+        option: ['JavaScript', 'Python', 'Java', 'C++'],
+        answer: 0
+    },
+
+    {
+        question: 'What is the most popular programming language?',
+        option: ['Python', 'JavaScript', 'Java', 'C++'],
+        answer: 1
+    },
+
+    {
+        question: 'Which is the largest country in the world?',
+        option: ['China', 'India', 'United States', 'Indonesia'],
+        answer: 0
+    },  
+
+]
